33daf2ce009353dd9911ce9aaf00b2bf7c0d3f8b
Gustavo Cayres <gustavocayrespublico@gmail.com>
2017-09-06 17:35:08 -0300

    hj

--- bug2/bug2.java
+++ bug2/bug2.java
@@ -4,106 +4,135 @@ import lejos.nxt.*;
 import lejos.util.Delay;
 
 public class bug2 {
+	private static Double[] position = {0.0, 0.0, 0.0};
+	private static Integer[] tacho = {0, 0};
+	private static double r = 2.8;
+	private static double axis = 11.2;
+	private static NXTMotor mb = new NXTMotor(MotorPort.B); //create instance of an unregulated motor in motor port B 
+	private static NXTMotor mc = new NXTMotor(MotorPort.C); //create instance of an unregulated motor in motor port C 
+	private static NXTRegulatedMotor rb = new NXTRegulatedMotor(MotorPort.B); //create instance of a regulated motor in motor port B 
+	private static NXTRegulatedMotor rc = new NXTRegulatedMotor(MotorPort.C); //create instance of a regulated motor in motor port C
+	private static LightSensor light = new LightSensor(SensorPort.S4);
+	private static int rotation = 0;
 
 	public static void main(String args[])  
   	{
-		NXTMotor mB = new NXTMotor(MotorPort.B); //create instance of an unregulated motor in motor port B 
-		NXTMotor mC = new NXTMotor(MotorPort.C); //create instance of an unregulated motor in motor port C 
-		NXTRegulatedMotor rb = new NXTRegulatedMotor(MotorPort.B); //create instance of a regulated motor in motor port B 
-		NXTRegulatedMotor rc = new NXTRegulatedMotor(MotorPort.C); //create instance of a regulated motor in motor port C
-		LightSensor light = new LightSensor(SensorPort.S4);
-		double r = 2.8;
-		double axis = 11.2;
-		Double[] position = new Double[3];
-		Integer[] tacho = new Integer[2];
-		int u_straight = 50;
-		int turn;
-		int light_measurement;
-		int kp = 6;
-		int var;
-		int previous_error = 0;
-		int error;
-		int kd = 3;
-
   		RConsole.openAny(0);
 
 		Button.waitForAnyPress();
 		
-		rb.rotate(256,true);
-		rc.rotate(-256);
+		/*rb.rotate(256,true);
+		rc.rotate(-256);*/
 
-  		position[0] = 0.0;
-  		position[1] = 0.0;
-  		position[2] = 0.0;
-
-		tacho[0] = 0;
-		tacho[1] = 0;
 		rb.suspendRegulation();
 		rc.suspendRegulation();
-		mB.setPower(30);
-		mC.setPower(30);
-		RConsole.println(""+light.getLightValue());
-		while(true) {
-			mB.forward();
-			mC.forward();
-			while(light.getLightValue() > 45) {
-				RConsole.println(""+light.getLightValue());
-				update_position(position, tacho, r, axis);
-			}
-			
-			while(position[1] < -.5 && position[1] > +.5){
-				light_measurement = light.getLightValue();
-				error = (45 - light_measurement);
-				turn = kp * error + kd * (previous_error - error);
-				var = u_straight - turn;
-				if(var > 99) {
-					var = 100;
-				}
-				if(var < -100) {
-					var = -100;
-				} 
-				mB.setPower(var);
-				var = u_straight + turn;
-				if(var > 99) {
-					var = 100;
-				}
-				if(var < -100) {
-					var = -100;
-				} 
-				mC.setPower(var);
-				previous_error = error;
-				update_position(position, tacho, r, axis);
-			}
+
+		while(position[0] < 100) {
+		//if(true){
+			follow_line_until_obstacle();
+			go_around_obstacle();
+			rotate_to_initial_direction();
 		}
 	}	
 
-	private static void update_position(Double[] position, Integer[] tacho, double r, double axis) {
-		int new_tacho_A = Motor.C.getTachoCount();
+	private static void update_position() {
+		int new_tacho_C = Motor.C.getTachoCount();
 		int new_tacho_B = Motor.B.getTachoCount();
-		double delta_s_r = Math.toRadians(new_tacho_A - tacho[0]) * r;
-		double delta_s_l = Math.toRadians(new_tacho_B - tacho[1]) * r;
+		double delta_s_l = Math.toRadians(new_tacho_C - tacho[0]) * r;
+		double delta_s_r = Math.toRadians(new_tacho_B - tacho[1]) * r;
 		double delta_s = (delta_s_l + delta_s_r)/2;
 		double delta_theta = (delta_s_r - delta_s_l)/axis;
+
 		position[0] += delta_s * (Math.cos(position[2] + delta_theta/2));
 		position[1] += delta_s * (Math.sin(position[2] + delta_theta/2));
 		position[2] += delta_theta; /*theta em radianos*/
-		tacho[0] = new_tacho_A;
+		tacho[0] = new_tacho_C;
 		tacho[1] = new_tacho_B;
 	}
-	private static void print_position(Double[] position){
+	private static void print_position(){
 		LCD.drawString(Double.toString(position[0]),0,0);
 		LCD.drawString(Double.toString(position[1]),0,1);
 		LCD.drawString(Double.toString(position[2]),0,2);
 		Delay.msDelay(200);
 		LCD.clear();
 	}
-		private static void print_position_console(Double[] position){
-		RConsole.println("x = "+position[0]);
+	private static void print_position_console(){
+		RConsole.println("x = "+position[0]+" y = "+position[1]);
 		//LCD.drawString(Double.toString(position[0]),0,0);
 		//LCD.drawString(Double.toString(position[1]),0,1);
 		//LCD.drawString(Double.toString(position[2]),0,2);
 		//Button.waitForAnyPress();
 		//LCD.clear();
+	}
+	private static void follow_line_until_obstacle(){
+		mb.setPower(25);
+		mc.setPower(25);
+		mb.forward();
+		mc.forward();
+		while(light.getLightValue() > 45 && position[0] < 100) {
+			update_position();
+		}
+	}
+	private static void go_around_obstacle(){
+		int u_straight = 20;
+		int turn;
+		int light_measurement;
+		int kp = 5;
+		int var;
+		int previous_error = 0;
+		int error;
+		int kd = 2;
+		boolean stop_cycling = false;
+
+		long start_time = System.currentTimeMillis();
+		
+		while(!stop_cycling) {
+			if (System.currentTimeMillis() - start_time > 2000 && Math.abs(position[1]) < 1) {
+				stop_cycling = true;
+			}
+
+			light_measurement = light.getLightValue();
+			error = (45 - light_measurement);
+			turn = kp * error + kd * (previous_error - error);
+			var = u_straight - turn;
+			if(var > 99) {
+				var = 100;
+			}
+			if(var < -100) {
+				var = -100;
+			} 
+			mb.setPower(var);
+			var = u_straight + turn;
+			if(var > 99) {
+				var = 100;
+			}
+			if(var < -100) {
+				var = -100;
+			} 
+			mc.setPower(var);
+			previous_error = error;
+			update_position();
+		}
+	}
+
+	private static void rotate_to_initial_direction() {
+		rb.stop(true);
+		rc.stop();
+		if(rotation == 0) {
+			rb.rotate(-250,true);
+			rc.rotate(250);
+		}
+		if(rotation == 1) {
+			rb.rotate(-120,true);
+			rc.rotate(120);
+		}
+		rotation = rotation + 1;
+		rb.stop(true);
+		rc.stop();
+		//regulated to unregulated
+		rb.suspendRegulation(); 
+		rc.suspendRegulation();
+
 	}
 
 
