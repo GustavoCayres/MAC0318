d9c7370789d9e40450a3c4682e23589739e7ee39
Gustavo Cayres <gustavocayrespublico@gmail.com>
2017-10-23 17:20:59 -0200

    leituras parte 7

---
+++ Projeto_7/map_generator.py
@@ -0,0 +1,53 @@
+import sys
+import math
+
+
+def polar2cartesian(x, y, t, r, delta_t):
+	point_x = x + r * math.cos(math.radians(t + delta_t))
+	point_y = y + r * math.sin(math.radians(t + delta_t))
+	
+	return point_x, point_y
+
+def points2lines(points):
+	return points2lines_rec(0, len(points) - 1, points);
+
+def points2lines_rec(i_a, i_b, points):
+	threshold = 20
+	lines = []
+
+	for i_p, point in enumerate(points):
+		if distance2line(i_a, i_b, i_p, points) > threshold:
+			lines += points2lines_rec(i_a, i_p, points[0:i_p+1])
+			lines += points2lines_rec(0, i_b - i_p, points[i_p:])
+
+	if len(lines) == 0:
+		lines.append((points[i_a], points[i_b]))
+
+	return lines
+
+
+def distance2line(i_a, i_b, i_p, points):
+	x_a, y_a = points[i_a]
+	x_b, y_b = points[i_b]
+	x_p, y_p = points[i_p]
+	numerator = abs((y_b - y_a) * x_p - (x_b - x_a) * y_p + (x_b * y_a) - (y_b * x_a))
+	denominator = math.sqrt((y_b - y_a)**2 + (x_b - x_a)**2)
+	return numerator/denominator
+
+def main():
+	cartesian_points = []
+	with open(sys.argv[1], 'r') as f:
+		for line in f:
+			values = line.split()
+
+			x, y, theta = float(values[0]), float(values[1]), float(values[2])
+			readings = values[3:]
+			for i, reading in enumerate(readings):
+				if (float(reading) < 255):
+					cartesian_points.append(polar2cartesian(x, y, theta, float(reading), -90 + (2 * i)))
+			lines = points2lines(cartesian_points)
+			for line in lines:
+				print("new Line(" + str(int(line[0][0])+100) + "," + str(int(line[0][1])+100) + "," + str(int(line[1][0])+100) + "," + str(int(line[1][1])+100) + "),")	
+
+if(__name__ == "__main__"):
+	main()
