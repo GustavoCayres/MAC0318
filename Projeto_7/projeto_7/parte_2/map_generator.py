c881e47eb1397a8829bc6cc8c336a2cad168e700
Gustavo Cayres <gustavocayrespublico@gmail.com>
2017-10-29 18:22:32 -0200

    Add stop to recursion

--- Projeto_7/projeto_7/parte_2/map_generator.py
+++ Projeto_7/projeto_7/parte_2/map_generator.py
@@ -3,51 +3,57 @@ import math
 
 
 def polar2cartesian(x, y, t, r, delta_t):
-	point_x = x + r * math.cos(math.radians(t + delta_t))
-	point_y = y + r * math.sin(math.radians(t + delta_t))
-	
-	return point_x, point_y
+    point_x = x + r * math.cos(math.radians(t + delta_t))
+    point_y = y + r * math.sin(math.radians(t + delta_t))
+
+    return point_x, point_y
+
 
 def points2lines(points):
-	return points2lines_rec(0, len(points) - 1, points);
+    return points2lines_rec(0, len(points) - 1, points)
+
 
 def points2lines_rec(i_a, i_b, points):
-	threshold = 20
-	lines = []
+    threshold = 20
+    lines = []
+    print(i_a, i_b)
+    for i_p in range(i_a, i_b + 1):
+        if distance2line(i_a, i_b, i_p, points) > threshold:
+            lines += points2lines_rec(i_a, i_p, points)
+            lines += points2lines_rec(i_p, i_b, points)
+            break
 
-	for i_p, point in enumerate(points):
-		if distance2line(i_a, i_b, i_p, points) > threshold:
-			lines += points2lines_rec(i_a, i_p, points[0:i_p+1])
-			lines += points2lines_rec(0, i_b - i_p, points[i_p:])
+    if len(lines) == 0:
+        lines.append((points[i_a], points[i_b]))
 
-	if len(lines) == 0:
-		lines.append((points[i_a], points[i_b]))
-
-	return lines
+    return lines
 
 
 def distance2line(i_a, i_b, i_p, points):
-	x_a, y_a = points[i_a]
-	x_b, y_b = points[i_b]
-	x_p, y_p = points[i_p]
-	numerator = abs((y_b - y_a) * x_p - (x_b - x_a) * y_p + (x_b * y_a) - (y_b * x_a))
-	denominator = math.sqrt((y_b - y_a)**2 + (x_b - x_a)**2)
-	return numerator/denominator
+    x_a, y_a = points[i_a]
+    x_b, y_b = points[i_b]
+    x_p, y_p = points[i_p]
+    numerator = abs((y_b - y_a) * x_p - (x_b - x_a) * y_p + (x_b * y_a) - (y_b * x_a))
+    denominator = math.sqrt((y_b - y_a) ** 2 + (x_b - x_a) ** 2)
+    return numerator / denominator
+
 
 def main():
-	cartesian_points = []
-	with open(sys.argv[1], 'r') as f:
-		for line in f:
-			values = line.split()
+    cartesian_points = []
+    with open(sys.argv[1], 'r') as f:
+        for line in f:
+            values = line.split()
 
-			x, y, theta = float(values[0]), float(values[1]), float(values[2])
-			readings = values[3:]
-			for i, reading in enumerate(readings):
-				if (float(reading) < 255):
-					cartesian_points.append(polar2cartesian(x, y, theta, float(reading), -90 + (2 * i)))
-			lines = points2lines(cartesian_points)
-			for line in lines:
-				print("new Line(" + str(int(line[0][0])+100) + "," + str(int(line[0][1])+100) + "," + str(int(line[1][0])+100) + "," + str(int(line[1][1])+100) + "),")	
+            x, y, theta = float(values[0]), float(values[1]), float(values[2])
+            readings = values[3:]
+            for i, reading in enumerate(readings):
+                if float(reading) < 255:
+                    cartesian_points.append(polar2cartesian(x, y, theta, float(reading), -90 + (2 * i)))
+            lines = points2lines(cartesian_points)
+            for l in lines:
+                print("new Line(" + str(int(l[0][0]) + 100) + "," + str(int(l[0][1]) + 100) + "," + str(
+                    int(l[1][0]) + 100) + "," + str(int(l[1][1]) + 100) + "),")
 
-if(__name__ == "__main__"):
-	main()
+
+if __name__ == "__main__":
+    main()
